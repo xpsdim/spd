411010e92ad5101721d74003fedabe27cc8fad76
dima.sukhorukoff <dima.sukhorukoff@outlook.com>
2018-09-13 16:01:39 +0300

    Login menu item added, 5000 port for developing hardcoded

--- Spd3/Program.cs
+++ Spd3/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Spd3
 {
@@ -17,8 +10,9 @@ namespace Spd3
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+			.UseStartup<Startup>()
+			.UseUrls("http://*:5000");
     }
 }
